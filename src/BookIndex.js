1e50fb9f94537f05ac823f57b62290501013f20b
Th13um <dthieum@gmail.com>
2018-08-25 19:15:07 +0200

    Correct Errors of routing

--- src/BookIndex.js
+++ src/BookIndex.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 class BookIndex extends Component {
      render() {
        return (
+         <div className="app">
            <div className="list-books">
              <div className="list-books-title">
                <h1>MyReads</h1>
