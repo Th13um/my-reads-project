d3625fde14ee969701db4c4aa6713e2e057ff828
Th13um <dthieum@gmail.com>
2018-08-26 13:45:01 +0200

    Test the app but have a problem with books cover

--- src/App.js
+++ src/App.js
@@ -6,28 +6,47 @@ import BookIndex from './BookIndex';
 import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
+
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: [] ,
+    searchBook: []
+  }
+
+  componentDidMount() {
+   BooksAPI.getAll().then((books) => {
+     this.setState({ books })
+     //console.log(this.state);
+    })
+  }
+
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(book => {
+      this.setState(state => ({
+        books: state.books.update
+      }))
+    })
   }
 
   render() {
     return (
-      <div>
-        <Route exact path='/' render={() => (
-          <BookIndex />
+      <div className="app">
+        <Route exact path='/' exact render={() => (
+          <BookIndex
+            books={this.state.books}
+            changeShelf={this.changeShelf}
+          />
         )} />
-        <Route path='/search' render={({ history }) => (
-          <BookSearch />
+        <Route exact path='/search' render={({ history }) => (
+          <BookSearch
+            changeShelf={this.changeShelf}
+            updateBooksList={this.updateBooksList}
+            searchBook={this.searchBook}
+          />
         )} />
-            </div>
+      </div>
     )
   }
 }
+
 
 export default BooksApp;
